cdfdcf7ce4f9d4a2d2adc40fe85567a892b4120f
scot0359 <43067695+scot0359@users.noreply.github.com>
2018-11-15 10:02:40 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,51 @@
 /* globals APIKEY */
+
+const movieDataBaseURL = "https://api.themoviedb.org/3/";
+let imageURL = null;
+let imageSizes = [];
+
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    console.log(APIKEY);
+   // console.log(APIKEY);
     addEventListeners();
+    getLocalStorageData()
 }
 
 function addEventListeners() {
 
+}
+
+function getLocalStorageData() {
+    // load image sizes and base url from local storage
+    
+    // doesn't exist
+    // the data is there but stale (1 hour old)
+    
+    getPosterSizesAndURL()
+    
+    // else it does exist and is less than 1 hour old
+    // load from local storage
+}
+
+function getPosterSizesAndURL(){
+    
+    let url = `${movieDataBaseURL}configuration?api_key=${APIKEY}`
+    
+    fetch(url)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+        imageURL = data.images.secure_base_url;
+        imageSizes = data.images.poster_sizes;
+        
+        console.log(imageURL);
+        console.log(imageSizes);
+    })
+    .catch(function(error){
+        alert(error);
+    });
+    
 }
